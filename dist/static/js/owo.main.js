481d7e6c82fc2e0371980d9bf4ab9d91abea7b0b
PUGE <100284685@qq.com>
2019-08-07 00:36:29 +0800

    save

--- dist/static/js/owo.main.js
+++ dist/static/js/owo.main.js
@@ -1,4 +1,4 @@
-// Wed Aug 07 2019 00:25:42 GMT+0800 (GMT+08:00)
+// Wed Aug 07 2019 00:29:26 GMT+0800 (GMT+08:00)
 
 /* 方法合集 */
 var _owo = {
@@ -198,7 +198,7 @@ owo.go = function (pageName, inAnimation, outAnimation, backInAnimation, backOut
       "forward": false
     }
   }
-  window.location.href = paramString + "#" + pageName
+  location.replace(paramString + "#" + pageName)
 }
 
 // url发生改变事件
